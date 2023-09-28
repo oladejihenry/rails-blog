c8ed21947097ae09fc58077af27741cb804170bc
Henry Oladeji <henryoladeji2007@gmail.com>
2023-09-29 00:55:59 +0100

    changes

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,7 +4,7 @@ class BlogPostsController < ApplicationController
     
     #index 
     def index
-        @blog_posts = BlogPost.all
+        @blog_posts = BlogPost.published
     end
 
     #show 
@@ -48,11 +48,11 @@ class BlogPostsController < ApplicationController
     private
 
     def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
+        params.require(:blog_post).permit(:title, :body, :published_at)
     end
 
     def set_blog_post
-        @blog_post = BlogPost.find(params[:id])
+        @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
